c5800ce989869e4ec726c887e268f6c13245d051
sakeyehp <sakeyehp@yahoo.co.uk>
2020-01-20 11:24:41 +0100

    using states

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = (props) => {
+    const [ counter, setCounter ] = useState(0)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+        <div>
+            <div>{counter}</div>
+            <div>
+            <button onClick={() => setCounter(counter + 1)}>
+                plus
+            </button>
+            </div>
+            <div>
+            <button onClick={() => setCounter(0)}>
+                reset
+            </button>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
