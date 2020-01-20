679b364384bd00775a13f12f398cb2c86eb05338
sakeyehp <sakeyehp@yahoo.co.uk>
2020-01-20 12:01:23 +0100

    Using States -2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+const Display = ({counter}) => 
+        <div>{counter}</div>
+
 const App = (props) => {
     const [ counter, setCounter ] = useState(0)
 
+    const setValue = (value) => setCounter(value)
+
+    // const setValue = (value) => {
+    //     return () => {
+    //         setCounter (value);
+    //     }
+    // }
+
     return (
         <div>
-            <div>{counter}</div>
-            <div>
-            <button onClick={() => setCounter(counter + 1)}>
-                plus
-            </button>
-            </div>
-            <div>
-            <button onClick={() => setCounter(0)}>
-                reset
-            </button>
-            </div>
+
+            <Display counter = {counter} />
+            <Button onClick={() => setValue(counter+1)}
+            // {/* <button onClick={setValue(counter+1)}> */}
+                text = 'plus'
+            />
+            <Button onClick={() => setValue(counter - 1)} 
+            text = 'minus'
+            />
+
+            <Button onClick={() => setValue(0)}
+            // {/* <button onClick={setValue(0)}> */}
+                text = 'reset'
+            />
         </div>
     )
 }
